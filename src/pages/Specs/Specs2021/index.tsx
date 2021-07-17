74e59752407de8c95b48dc66e6c8c614a85fbc50
Stone Tao <stonezt2019@gmail.com>
2021-07-16 23:52:37 -0400

    table of contents working

--- src/pages/Specs/Specs2021/index.tsx
+++ src/pages/Specs/Specs2021/index.tsx
@@ -1,10 +1,21 @@
 import DefaultLayout from '../../layouts/default';
 import MarkdownIt from 'markdown-it';
 import './index.css';
-import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import {
+  createStyles,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+  Theme,
+  Typography,
+  useMediaQuery,
+} from '@material-ui/core';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { HashLink } from 'react-router-hash-link';
 const md = new MarkdownIt({ html: true, linkify: true });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -31,6 +42,8 @@ const getTimeDifferenceString = (now: Date, start: Date): string => {
 
 const Specs2021Page = () => {
   const [content, setContent] = useState('');
+  const [h3s, setH3s] = useState<{ text: string; href: string; el: any }[]>([]);
+  const [h3Active, setH3Active] = useState(0);
   const useStyles = makeStyles((theme: Theme) => {
     return createStyles({
       page: {
@@ -55,17 +68,40 @@ const Specs2021Page = () => {
         }
       });
       const h2elems = document.getElementsByTagName('h2');
+
       for (let i = 0; i < h2elems.length; i++) {
         h2elems[i].className =
           'MuiTypography-root MuiTypography-h2 MuiTypography-colorTextPrimary';
       }
+
       const h3elems = document.getElementsByTagName('h3');
+      const h3s: any[] = [];
       for (let i = 0; i < h3elems.length; i++) {
         const href = h3elems[i].textContent;
         h3elems[i].innerHTML += `<span class="anchor" id="${href}"></span>`;
         h3elems[i].className =
           'MuiTypography-root MuiTypography-h3 MuiTypography-colorTextPrimary';
+        h3s.push({ text: h3elems[i].textContent, href, el: h3elems[i] });
+        // h3elems[i].previousElementSibling;
       }
+      setH3s(h3s);
+      document.addEventListener(
+        'scroll',
+        () => {
+          for (let i = 0; i < h3s.length; i++) {
+            const h3 = h3s[i + 1 - 1];
+            // if (h3.el.previousElementSibling) {
+            if (isInViewport(h3.el)) {
+              setH3Active(i);
+              break;
+            }
+            // }
+          }
+        },
+        {
+          passive: true,
+        }
+      );
       const h4elems = document.getElementsByTagName('h4');
       for (let i = 0; i < h4elems.length; i++) {
         const href = h4elems[i].textContent;
@@ -80,14 +116,55 @@ const Specs2021Page = () => {
       }
     });
   }, []);
+  function isInViewport(element: any) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  const smallSize = useMediaQuery('(max-width: 768px)');
   return (
     <DefaultLayout>
       <div className={`${classes.page} Specs2021Page`}>
         <Typography variant="h2"></Typography>
-        <div
-          className="container"
-          dangerouslySetInnerHTML={{ __html: content }}
-        ></div>
+        <div className="container">
+          <Grid container spacing={4} className="specs-grid">
+            {!smallSize && (
+              <Grid item xs={2}>
+                <List
+                  className="toc-bar"
+                  component="nav"
+                  aria-label={h3s.map((h3) => h3.text).join(' ')}
+                >
+                  {h3s.map((h3, i) => {
+                    return (
+                      <HashLink
+                        to={`#${h3.href}`}
+                        className={`toc-item ${h3Active === i && 'active'}`}
+                        key={h3.text}
+                      >
+                        <ListItem button>
+                          <ListItemText
+                            className="toc-text"
+                            primary={h3.text}
+                          />
+                        </ListItem>
+                      </HashLink>
+                    );
+                  })}
+                </List>
+              </Grid>
+            )}
+            <Grid item xs={smallSize ? 12 : 10}>
+              <div dangerouslySetInnerHTML={{ __html: content }}></div>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </DefaultLayout>
   );
